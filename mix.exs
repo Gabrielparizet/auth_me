00c863f2047158589119e154d855de859fb2c6e8
Gabrielparizet <parizetgabriel@gmail.com>
2024-06-14 10:36:51 +0200

    build(deps): guardian and argon2

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule AuthMe.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:guardian, "~> 2.3.2"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
